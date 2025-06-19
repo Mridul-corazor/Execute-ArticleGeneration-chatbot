02fa04672020e99978235bf95dd48b260626e0c1
Mridul-corazor <mridul@corazor.com>
2025-06-19 10:00:22 +0530

    Update write_article.py

--- write_article.py
+++ write_article.py
@@ -1,9 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from llm_service import call_gemini,close_conversation
 from pydantic import BaseModel
 import logging
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
+origins = [ 
+                 "https://html-starter-f9sl5f5qe-mridul-corazors-projects.vercel.app",
+                 "https://html-starter-b7nfda735-mridul-corazors-projects.vercel.app",
+                 "http://192.168.31.68:5500",
+                 "http://localhost:5173", 
+                 "http://127.0.0.1:5173",
+                 "http://localhost:3000", 
+                 "http://127.0.0.1:3000",
+                 "https://www.executepartners.com",
+                 "https://execute-partners-community.vercel.app"
+             ]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class UserInput(BaseModel):
     message: str
     session_id: str
